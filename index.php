16da4bebc89f036067092e04a105c2cdcc05540c
SV-Zanshin <Zanshin@SV-Zanshin.Com>
2018-10-21 10:24:27 +0100

    new file

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<?php /****************************************************************************************************************************
+** Main site homepage                                                                                                            **
+**                                                                                                                               **
+** Date       Change                                                                                                             **
+** ========== ================================================================================================================   **
+** 2017-04-01 Added/changed site keywords to remove unused terms                                                                 **
+** 2017-03-23 Added/changed site keywords to remove duplicates and add all countries visited                                     **
+** 2017-02-17 Added link to most recent blog page                                                                                **
+** 2017-01-28 Changed text around                                                                                                **
+** 2017-01-19 Initial page definition                                                                                            **
+********************************************************************************************************************************/?>
+<html lang="en-US">
+  <head>
+    <title>
+      SV-Zanshin GitHub home
+    </title>
+    <meta name="Description" content="Homepage for the sailing yacht ZANSHIN - blogs, diary, information, pictures, and much more"/>
+    <meta name="Keywords"    content="Zanshin,SV-Zanshin,yacht,jeanneau,57,sailboat,Caribbean,solo,
+                                     singlehand,circumnavigation,travel,pictures,blog,sloop,blog,diary,modifications,Kitts,Nevis,
+                                     sailing,BVI,USVI,Saint Martin,Barths,Sint Maarten,Antigua,Barbuda,Dominica,pictures,scuba,Guadeloupe" />
+    <meta name="page-topic"  content="Sailing Vessel Zanshin Home and Landing page"/>
+    <section id="banner"> <!-- Banner -->
+      <div class="content"> <!-- Content -->
+        <header>
+          <h2>Greetings from Zanshin (&#27531; &#24515;)</h2>
+        </header>
+        </div> <!-- of Content division -->
+    </section> <!-- end of banner section -->
+    <p>&#8220;Zanshin&#8221; is me</p>
+    <p>Please enjoy browsing the site and, for those who find themselves sailing in unknown waters:</p>
+    <p style="text-align:center">
+      <span class="handbreitBig">&#8220;Immer eine Handbreit Wasser unter dem Kiel.&#8221;</span>
+      <br><span class="handbreitSmall">(May there always be a handsbreadth of water beneath your keel)</span>
+    </p>
+  </body>
+</html>
